195b192ec625b36b2fe108c69f3b0b14a62edec3
Bhatt Umang <bhatt.umang7@gmail.com>
2024-09-17 22:23:52 +0530

    Requesting disk permissions

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -7,5 +7,12 @@ namespace SafariBooksDownload
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            // Pass the result to the PermissionHelper
+            PermissionHelper.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
